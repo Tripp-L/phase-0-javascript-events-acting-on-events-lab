380e06b16d0d5376a1b08872a33ec19d991ff2b3
trippl <tlaursen8@yahoo.com>
2024-04-03 18:13:56 -0400

    JS events acting on events lab

--- index.js
+++ index.js
@@ -1,28 +1,67 @@
 const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "yellow";
+
 function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
   }
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-  });
-  function moveDodgerRight() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left + 1}px`;
-    }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
   }
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "ArrowLeft") {
-      moveDodgerLeft();
-    }
-});
+})
+
+function moveDodgerRight() {
+  const leftNumbers =dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+  dodger.style.left = `${left + 1}px`
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+}) 
+
+// Added moveDodgerUp and moveDodgerDown
+
+function moveDodgerUp() {
+  const topNumbers = dodger.style.top.replace("px", "");
+  const top = parseInt(topNumbers, 10);
+
+  if (top > 0) {
+    dodger.style.top = `${top - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowUp") {
+    moveDodgerUp();
+  }
+})
+
+function moveDodgerDown() {
+  const topNumbers = dodger.style.top.replace("px", "");
+  const top = parseInt(topNumbers, 10);
+
+  if (top > 0) {
+    dodger.style.top = `${top + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowDown") {
+    moveDodgerDown();
+  }
+})
+
 
